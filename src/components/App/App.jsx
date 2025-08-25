3f7a54e3e075bf72361a3e7107d38ce38e7bdb2d
bndnt <67003594+bndnt@users.noreply.github.com>
2025-08-24 23:37:19 -0400

    component SearchBar + library toast for errors

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,12 @@
+import SearchBar from "../SearchBar/SearchBar";
 import "./App.module.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,49 @@
+import css from "./SearchBar.module.css";
+import { FaSearch } from "react-icons/fa";
+import toast, { Toaster } from "react-hot-toast";
+import { useState } from "react";
+export default function SearchBar({ onSubmit = () => {} }) {
+  const [notification, setNotification] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const inputValue = form.elements.word.value;
+    if (form.elements.word.value.trim() === "") {
+      setNotification(true);
+    } else {
+      setNotification(false);
+      onSubmit(e);
+    }
+
+    form.reset();
+    // console.log(inputValue);
+  };
+  const notify = () => {
+    // toast.dismiss();
+    toast.remove();
+    toast.error("Type at least 1 symbol");
+  };
+
+  return (
+    <header className={css.header}>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <input
+          name="word"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          className={css.input}
+        />
+        <button onClick={notify} className={css.btn} type="submit">
+          <span>
+            <FaSearch />
+          </span>
+          <span className={css.searchSpanText}>Search</span>
+        </button>
+      </form>
+      {notification && <Toaster position="top-left" reverseOrder={false} />}
+    </header>
+  );
+}
